342101f549335fd9c9268f18557e35c33b9771a8
mathewsageorge <mathewsgeorge202@gmail.com>
2024-06-20 16:36:46 +0530

    set email priority to high

--- server.js
+++ server.js
@@ -119,8 +119,8 @@ const sendEmail = async (email, subject, text) => {
         html: `
             <div style="font-family: Arial, sans-serif; font-size: 16px;">
                 <p><strong>Important:</strong> ${text}</p>
-                <p style="font-size: 18px; font-weight: bold;">Best regards,</p>
-                <p style="font-size: 18px; font-weight: bold;">NFCAMS Team</p>
+                <p style="font-size: 12px; ">Best regards,</p>
+                <p style="font-size: 12px; ">NFCAMS Team</p>
                 <hr>
                 <footer style="font-size: 14px; color: grey;">
                     <p>Copyright © 2024 NFCAMS. All rights reserved.</p>
